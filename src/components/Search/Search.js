475e4d0e255dfcf28a31e583bc416e3577625694
Dellawall <odetodetola@gmail.com>
2023-03-28 00:46:28 +0100

    updated search method and action test

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -11,9 +11,12 @@ const Search = () => {
             <BsList size={20}/>
         </div>
         <div className='form-select'>
-        <form name='search' action='get' href='google.com'>
-            <input placeholder='search items' />
-            <button><AiOutlineSearch className='list-icons' /></button>
+        <form name="search" role="search" method="get" action="https://www.jodaxattorneys.com.ng/">
+        {/* <input class="search-field" placeholder="Search "  /> */}
+            <input type="search" placeholder='search items &hellip;' name="search" />
+            
+            {/* <input  class="search-submit" /> */}
+            <button type="submit" value="Search"><AiOutlineSearch className='list-icons' /></button>
         </form>
         <select>
             <option>List price</option>
